ff5c8479bbd092dfd15835c04ad1be11ffb7def3
Anurag singh <anusingh99361634@gmail.com>
2026-01-27 00:30:28 +0530

    travel website

---
+++ db.js
@@ -0,0 +1,11 @@
+const DATABASE = {
+    'tw_travel_destination':{
+        '0_agra':{
+            'id':'o_agra',
+            'location':'Agra, India',
+            'distance':184,
+            'cost':10812
+        }
+    }
+}
+
